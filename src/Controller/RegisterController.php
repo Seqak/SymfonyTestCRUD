7d8ea42a3040c39ecfc04d7aa2324acc95ac1eaa
Kacper <kacperu770@gmail.com>
2020-08-09 20:31:05 +0200

    Registration form and roles added

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+/**
+ * @Route("/registration", name="registration.")
+ */
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/", name="register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+
+        $form = $this->createFormBuilder()
+            ->add('email')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+            ])
+            ->add('register', SubmitType::class)
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted())
+        {
+            $data = $form->getData();
+            $user = new User();
+
+            $user->setEmail($data['email']);
+            $user->setPassword(
+                $passwordEncoder->encodePassword($user, $data['password'])
+            );
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('app_login'));
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
